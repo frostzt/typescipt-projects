fb9b91fcde4cccec587a3b3db2f189695e835d61
frostzt <aidenfrostbite@gmail.com>
2021-07-07 21:07:46 +0530

    setting up Map and wrapping it

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,13 +1,6 @@
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
 const company = new Company();
-
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
