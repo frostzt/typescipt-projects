5f7d015b042b4b3435997a1c61e6d04a258aa755
frostzt <aidenfrostbite@gmail.com>
2021-07-07 21:04:43 +0530

    setting up Map and wrapping it

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -4,4 +4,10 @@ import { Company } from "./Company";
 const user = new User();
 const company = new Company();
 
-console.log(user, company);
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
