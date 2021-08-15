357e01a1f8cfc2169da8fd59455149c3fc495fda
frostzt <aidenfrostbite@gmail.com>
2021-08-15 09:22:30 +0530

    creating bubble sort algorithm using ts

---
+++ sorting/src/index.ts
@@ -0,0 +1,22 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const array = new Sorter([10, 3, -5, 2, 9]);
+array.sort();
+
+console.log(array);
