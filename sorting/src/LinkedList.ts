affd3cfffc421e7e602021b0c4349d8e6f4e35a5
frostzt <aidenfrostbite@gmail.com>
2021-09-16 16:33:42 +0530

    created linkedlist sorting

---
+++ sorting/src/LinkedList.ts
@@ -0,0 +1,88 @@
+class LinkedListNode {
+  next: LinkedListNode | null = null;
+
+  constructor(public data: number) {}
+}
+
+export class LinkedList {
+  head: LinkedListNode | null = null;
+
+  add(data: number): void {
+    const node = new LinkedListNode(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
+  }
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+
+    return length;
+  }
+
+  at(index: number): LinkedListNode {
+    if (!this.head) {
+      throw new Error("Index out of bounds");
+    }
+
+    let counter = 0;
+    let node: LinkedListNode | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+
+    throw new Error("Index out of bounds");
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error("List is empty");
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+
+    let node: LinkedListNode | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,22 +1,44 @@
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+
+interface Sortable {
+  length: number;
+  swap(leftIndex: number, rightIndex: number): void;
+  compare(leftIndex: number, rightIndex: number): boolean;
+}
+
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
         }
       }
     }
   }
 }
 
-const array = new Sorter([10, 3, -5, 2, 9]);
-array.sort();
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
 
-console.log(array);
+// const charactersCollection = new CharactersCollection("Xaayb");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-100);
+linkedList.add(120);
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
